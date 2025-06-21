441d080f9b4b6d69d833dc89402ce8797c282a22
Luiz Miguel Toller Marconatto <141162499+MiguelToller@users.noreply.github.com>
2025-06-21 13:15:20 -0300

    Add Produtos-MVC

---
+++ Produtos-MVC/ProjectMVC/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectMVC.Models;
+
+namespace ProjectMVC.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ Produtos-MVC/ProjectMVC/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectMVC.Models
+{
+    public class Produto
+    {
+        [Key] // Define que "Id" é a chave primária
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do produto é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O preço do produto é obrigatório")]
+        [Range(0.01, 9999.99, ErrorMessage = "O preço deve estar entre 0.01 e 9999.99")]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Preco { get; set; }
+    }
+}
